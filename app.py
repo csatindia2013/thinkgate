3437057eb97d19e710a543c702361f9faf219f7c
csatindia2013 <40131283+csatindia2013@users.noreply.github.com>
2025-04-29 01:52:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,4 +115,6 @@ def get_youtube_embed(query):
         return None
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
